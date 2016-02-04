7aa5920805b1718e60e1483c5ff207ab992bd382
xlwc350 <antoine.barrois1@gmail.com>
2016-02-04 13:17:20 +0100

    premier commit

---
+++ bddandroid.php
@@ -0,0 +1,31 @@
+<?php
+$con=mysqli_connect("localhost","root","","my_db");
+if (mysqli_connect_errno($con))
+{
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$username = $_GET['Username'];
+$password = $_GET['Password'];
+$result = mysqli_query($con,"SELECT Role FROM table1 where Username='$username' and Password='$password'");
+$row = mysqli_fetch_array($result);
+$data = $row[0];
+
+
+if($data){
+echo $data;
+}
+if (mysqli_connect_errno($con))
+{
+   echo "Failed to connect to MySQL: " . mysqli_connect_error();
+}
+$username = $_POST['username'];
+$password = $_POST['password'];
+$result = mysqli_query($con,"SELECT Role FROM table1 where Username='$username' and Password='$password'");
+$row = mysqli_fetch_array($result);
+$data = $row[0];
+
+if($data){
+echo $data;
+}
+mysqli_close($con);
+?>
