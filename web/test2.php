902c580c64c9e3ff7f7774b2f2d0c2efef74c55f
xlwc350 <antoine.barrois1@gmail.com>
2016-02-18 11:15:47 +0100

    simulation json

---
+++ web/test2.php
@@ -0,0 +1,4 @@
+<?php
+header('Content-Type: application/json');
+?>
+{"conge":[{"id_employe":"1","id_conge":"4","datedebut":"2016-02-11","datefin":"2016-02-18","motif":"CIF"},{"id_employe":"1","id_conge":"5","datedebut":"2016-02-19","datefin":"2016-02-22","motif":"RTT"}],"success":1}
